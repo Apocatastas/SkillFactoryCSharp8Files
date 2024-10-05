5b828b6d9ee7f1076ba1e05a3bb21c0e30ba2408
ɐllɐ <molchanova.a.a@gmail.com>
2024-10-05 22:12:46 +0300

    task 8.1.4

---
+++ SkillFactoryCSharp8Files/Program.cs
@@ -0,0 +1,33 @@
+﻿
+///Придумайте простой класс, который будет предоставлять информацию
+///об установленном в системе диске. 
+///Нужны свойства, чтобы хранить: имя диска, объём, свободное место.
+///Свойства инициализируются при создании нового объекта в методе-конструкторе.
+///
+
+
+using System;
+
+class Program
+{
+    public class Disc
+    {
+        public string Name { get; }
+        public long Volume { get; }
+        public long FreeSpace { get; }
+
+        public Disc(string discName, long discVolume, long discFreeSpace)
+        {
+            Name = discName;
+            Volume = discVolume;
+            FreeSpace = discFreeSpace;
+        }
+
+    }
+
+
+    public static void Main(string[] args)
+    {
+
+    }
+}
