d9cceba6080be7612f4333a1ec23aeb7dce1219c
ɐllɐ <molchanova.a.a@gmail.com>
2024-10-07 20:51:54 +0300

    task 8.1.5

---
+++ SkillFactoryCSharp8Files/Disc.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace SkillFactoryCSharp8Files
+{
+    public class Disc
+    {
+        public string Name { get; }
+        public long Volume { get; }
+        public long FreeSpace { get; }
+
+        public Disc(string discName, long discVolume, long discFreeSpace)
+        {
+            Name = discName;
+            Volume = discVolume;
+            FreeSpace = discFreeSpace;
+        }
+
+        Dictionary<string, Folder> Folders = new Dictionary<string, Folder>();
+
+        public void CreateFolder(string name)
+        {
+            Folders.Add(name, new Folder());
+        }
+
+    }
+}
+
---
+++ SkillFactoryCSharp8Files/Folder.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace SkillFactoryCSharp8Files
+{
+    public class Folder
+    {
+        public List<string> Files { get; set; } = new List<string>();
+
+        
+    }
+}
+
