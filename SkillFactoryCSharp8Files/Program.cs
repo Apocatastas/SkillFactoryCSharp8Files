879027040bc34a50add7ccc92f833802be5c7857
ɐllɐ <molchanova.a.a@gmail.com>
2024-10-08 13:49:55 +0300

    task 8.3.2

--- SkillFactoryCSharp8Files/Program.cs
+++ SkillFactoryCSharp8Files/Program.cs
@@ -1,26 +1,40 @@
 ﻿using System;
+using System.IO;
 using System.Reflection;
 
 class Program
 {
+    
     static void Main(string[] args)
     {
-        ShowSelfContent();
+        string filePath = @"/Users/apocatastas/Projects/SkillFactoryCSharp8Files/SkillFactoryCSharp8Files/Program.cs"; // Укажем путь
 
+        AddLastRunTimecode(filePath);
+        ShowSelfContent(filePath);
+        
         Console.ReadKey();
     }
 
-    static void ShowSelfContent()
+    static void ShowSelfContent(string filePath)
     {
 
-        string filePath = @"/Users/apocatastas/Projects/SkillFactoryCSharp8Files/SkillFactoryCSharp8Files/Program.cs"; // Укажем путь
-
+        
         // Откроем файл и прочитаем его содержимое
         using (StreamReader sr = File.OpenText(filePath))
         {
             string str = "";
             while ((str = sr.ReadLine()) != null)
                 Console.WriteLine(str);
+        }
+    }
+
+    static void AddLastRunTimecode(string filePath)
+    {
+
+        using (StreamWriter sw = File.AppendText(filePath))
+
+        {
+            sw.WriteLine($"// Время запуска: {DateTime.Now}");
         }
     }
           
