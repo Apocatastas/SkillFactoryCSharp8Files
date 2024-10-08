da6eeff88a4914d2e28da494b4a40bfc345ec45a
ɐllɐ <molchanova.a.a@gmail.com>
2024-10-08 13:30:04 +0300

    task 8.3.1

--- SkillFactoryCSharp8Files/Program.cs
+++ SkillFactoryCSharp8Files/Program.cs
@@ -5,62 +5,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        ObjectCounter();
-        
+        ShowSelfContent();
 
+        Console.ReadKey();
     }
 
-    static void ObjectCounter()
+    static void ShowSelfContent()
     {
-        try
+
+        string filePath = @"/Users/apocatastas/Projects/SkillFactoryCSharp8Files/SkillFactoryCSharp8Files/Program.cs"; // Укажем путь
+
+        // Откроем файл и прочитаем его содержимое
+        using (StreamReader sr = File.OpenText(filePath))
         {
-
-            DirectoryInfo dirInfoNew = new DirectoryInfo(@"/Users/apocatastas/Desktop/testFolder");
-            if (!dirInfoNew.Exists)
-                dirInfoNew.Create();
-
-
-            string newPath = "/Users/apocatastas/Library/Mobile Documents/.Trash/";
-
-            if (dirInfoNew.Exists && !Directory.Exists(newPath))
-               dirInfoNew.MoveTo(newPath);
-
-
-           // dirInfoNew.Delete(true); // Удаление со всем содержимым
-            Console.WriteLine("Каталог перемещен в корзину");
-            
-        }
-        catch (Exception e)
-        {
-            Console.WriteLine(e.Message);
-        }
-
-     Console.ReadKey();
-
-    }
-
-
-    static void GetCatalogs()
-    {
-        string dirName = @"/"; // Прописываем путь к корневой директории MacOS (для Windows скорее всего тут будет "C:\\")
-        if (Directory.Exists(dirName)) // Проверим, что директория существует
-        {
-            Console.WriteLine("Папки:");
-            string[] dirs = Directory.GetDirectories(dirName);  // Получим все директории корневого каталога
-
-            foreach (string d in dirs) // Выведем их все
-                Console.WriteLine(d);
-
-            Console.WriteLine();
-            Console.WriteLine("Файлы:");
-            string[] files = Directory.GetFiles(dirName);// Получим все файлы корневого каталога
-
-            foreach (string s in files)   // Выведем их все
-                Console.WriteLine(s);
-
-            Console.ReadKey();
+            string str = "";
+            while ((str = sr.ReadLine()) != null)
+                Console.WriteLine(str);
         }
     }
+          
 
     
 }
