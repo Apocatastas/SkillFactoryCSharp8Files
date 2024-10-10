effdb97544297851832bc1d1fef80c206c8c6a8c
ɐllɐ <molchanova.a.a@gmail.com>
2024-10-10 17:00:07 +0300

    Task 4 initial commit

---
+++ Task4/Student.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Text.Json;
+
+namespace Task4
+{
+    [Serializable]
+    public class Student
+    {
+        public static string serializedPath = "/Users/apocatastas/Projects/SkillFactoryCSharp8Files/Task4/Resources/students.dat";
+        public static string deserializedPath = "/Users/apocatastas/Desktop/";
+
+        public string Name { get; set; }
+        public string Group { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public decimal AverageScore { get; set; }
+
+        public Student(string name, string group, DateTime date, decimal score)
+        {
+            Name = name;
+            Group = group;
+            DateOfBirth = date;
+            AverageScore = score;
+        }
+
+        public void Serialize(string path)
+        {
+            var options = new JsonSerializerOptions { WriteIndented = true };
+            var jsonString = JsonSerializer.Serialize(this, options);
+            File.WriteAllText(path, jsonString);
+        }
+
+        public void Deserialize(string path)
+        {
+            var jsonString = File.ReadAllText(path);
+            var studentList = JsonSerializer.Deserialize<Student>(jsonString);
+
+        }
+    }
+}
+
--- Task4/Task4.cs
+++ Task4/Task4.cs
@@ -1,1 +1,29 @@
-﻿
+﻿using System;
+using System.IO;
+using System.Text.Json;
+namespace Task4
+{
+    /// <summary>
+    /// Ваша программа должна:
+    ///
+    /// Cчитать данные о студентах из файла;
+    /// ✓  Создать на рабочем столе директорию Students.
+    /// Внутри раскидать всех студентов из файла по группам
+    ///   (каждая группа-отдельный текстовый файл), в файле группы студенты
+    ///   перечислены построчно в формате "Имя, дата рождения, средний балл".
+    /// </summary>
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Объект для сериализации
+            Student student = new Student("","",DateTime.Now, 12345);
+            DirectoryInfo studDir = new DirectoryInfo(Student.deserializedPath + "Students/");
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
